b8a6bf2e11df2493f1002a577f350d6556e41f8d
koms <komsldt@gmail.com>
2025-01-17 18:39:18 +0400

    removed out

--- out/consts.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Field = exports.Key = exports.TableName = void 0;
-exports.TableName = 'my_table';
-exports.Key = { hash: { S: 'my_key' } };
-exports.Field = 'my_field';
